9d2916a2bca2f30dfb15146c1724642d08d2e333
henc0re <hencore.dev@protonmail.com>
2020-10-05 17:27:46 +0200

    style single article + add comment

--- templates/form_comment.php
+++ templates/form_comment.php
@@ -5,16 +5,15 @@ $submit = $route === 'addComment' ? 'Ajouter' : 'Mettre à jour';
 
 <form method="post" action="../public/index.php?route=<?= $route; ?>&articleId=<?= htmlspecialchars($article->getId()); ?>">
     <div class="form-group">
-        <label for="pseudo">Pseudo</label>
+        <label for="pseudo"></label>
         <br>
-        <input class="form-control" type="text" id="pseudo" name="pseudo" value="<?= isset($post) ? htmlspecialchars($post->get('pseudo')): ''; ?>">
+        <input class="form-control" type="text" id="pseudo" name="pseudo" value="<?= $this->session->get('pseudo') ?>"readonly>
         <br>
         <?= isset($errors['pseudo']) ? $errors['pseudo'] : ''; ?>
         <label for="content">Message</label><br>
         <textarea class="form-control" id="content" name="content"><?= isset($post) ? htmlspecialchars($post->get('content')): ''; ?></textarea>
         <br>
         <?= isset($errors['content']) ? $errors['content'] : ''; ?>
-        <br>
         <input class="btn btn-primary" type="submit" value="<?= $submit; ?>" id="submit" name="submit">
     </div>
 </form>
--- templates/single.php
+++ templates/single.php
@@ -4,16 +4,16 @@
         <h2><?= htmlspecialchars($article->getTitle());?></h2>
     </div>
     <div class="card-body">
-        <p class="card-text"><?= htmlspecialchars($article->getContent());?></p>
+        <p class="card-text text-justify"><?= htmlspecialchars($article->getContent());?></p>
     </div>
     <div class="card-footer text-muted">
-        <em>Créé le : <?= htmlspecialchars($article->getCreatedAt());?></em>
+        <em>Posté le : <?= htmlspecialchars($article->getCreatedAt());?> par <?= htmlspecialchars($article->getAuthor());?></em>
     </div>
     <?php
     if ($this->session->get('role') === 'admin')
     {
         ?>
-        <div class="actions card-footer">
+        <div class="actions card-footer d-flex justify-content-end">
             <a class="btn btn-outline-info" href="../public/index.php?route=editArticle&articleId=<?= $article->getId(); ?>">Modifier</a>
             <a class="btn btn-danger" href="../public/index.php?route=deleteArticle&articleId=<?= $article->getId(); ?>">Supprimer</a>
         </div>
@@ -23,39 +23,67 @@
 </div>
 <br>
 <div id="comments">
-    <h3>Commentaires</h3>
+    <hr>
+    <h3 class="text-center">Commentaires</h3>
     <?php
     foreach ($comments as $comment)
     {
         ?>
-        <h4><?= htmlspecialchars($comment->getPseudo());?></h4>
-        <p><?= htmlspecialchars($comment->getContent());?></p>
-        <p class="text-muted">Posté le <?= htmlspecialchars($comment->getCreatedAt());?></p>
-        <?php
-        if($comment->isFlag()) {
-            ?>
-            <p class="alert alert-warning">Ce commentaire a déjà été signalé.</p>
-            <?php
-        } 
-        else 
-        {
-            ?>
-            <p><a href="../public/index.php?route=flagComment&commentId=<?= $comment->getId(); ?>">Signaler le commentaire</a></p>
-            <?php
-        }
-        ?>
+        <div class="card">
+            <div class="card-header">
+                <h4><?= htmlspecialchars($comment->getPseudo());?></h4>
+            </div>
+            <div class="card-body">
+                <p><?= htmlspecialchars($comment->getContent());?></p>
+                <em class="text-muted">Posté le <?= htmlspecialchars($comment->getCreatedAt());?></em>
+                <div class="signal-comment">
+                    <?php
+                    if($comment->isFlag()) {
+                        ?>
+                        <p class="alert alert-warning">Ce commentaire a déjà été signalé.</p>
+                        <?php
+                    } 
+                    else 
+                    {
+                        ?>
+                        <p><a class="btn btn-warning" href="../public/index.php?route=flagComment&commentId=<?= $comment->getId(); ?>">Signaler le commentaire</a></p>
+                        <?php
+                    }
+                    ?>
+                    <?php
+                    if ($this->session->get('role') === 'admin')
+                    {
+                        ?>
+                        <div class="actions card-footer d-flex justify-content-end">
+                            <a class="btn btn-outline-success" href="../public/index.php?route=unflagComment&commentId=<?= $comment->getId(); ?>">Désignaler</a>
+                            <a class="btn btn-outline-secondary" href="../public/index.php?route=deleteComment&commentId=<?= $comment->getId(); ?>">Modifier</a>
+                            <a class="btn btn-danger" href="../public/index.php?route=deleteArticle&articleId=<?= $article->getId(); ?>">Supprimer</a>
+                        </div>
+                    <?php
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
         <br>
-        <hr>
-        <h3>Ajouter un commentaire</h3>
+    <?php    
+    }
+    ?>
+    <br>
+    <h4 class="text-center"><u>Ajouter un commentaire</u></h4>
+    <?php
+    if ($this->session->get('pseudo')) 
+    {
+        include('form_comment.php');
+    }
+    else
+    {
+        ?>
+        <em>vous devez être connecté pour poster un commentaire.</em>
+        <br>
+        <a href="../public/index.php?route=login" class="btn btn-light border">Connexion</a>
+        <a href="../public/index.php?route=register" class="btn btn-light border">Inscription</a>
         <?php
-        if ($this->session->get('pseudo')) 
-        {
-            include('form_comment.php');
-        }
-        else
-        {
-            echo '<em>vous devez être connecté pour poster un commentaire.</em>';
-        }
     }
     ?>
 </div>
