a2575e6b4c9ba3bc86860e7f004443c0be206b24
henc0re <hencore.dev@protonmail.com>
2020-10-11 04:25:02 +0200

    check pseudo contains only letters

--- src/constraint/Constraint.php
+++ src/constraint/Constraint.php
@@ -7,19 +7,29 @@ class Constraint
     public function notBlank($name, $value)
     {
         if(empty($value)) {
-            return '<p>Le champ '.$name.' saisi est vide</p>';
+            return '<p>Le champ '.$name.' saisi est vide.</p>';
         }
     }
+
     public function minLength($name, $value, $minSize)
     {
         if(strlen($value) < $minSize) {
             return '<p>Le champ '.$name.' doit contenir au moins '.$minSize.' caractères</p>';
         }
     }
+
     public function maxLength($name, $value, $maxSize)
     {
         if(strlen($value) > $maxSize) {
             return '<p>Le champ '.$name.' doit contenir au maximum '.$maxSize.' caractères</p>';
+        }
+    }
+
+    public function onlyLetters($name, $value)
+    {
+        if (!ctype_alpha($value)) 
+        {
+            return '<p>Votre '.$name.' ne peut contenir que des lettres. <br> '.$value.' n\'est donc pas valide.</p>';
         }
     }
 }
